d647f8baaef9c6aa14405052e6706742d6f7bce3
krista2811 <krista2811@naver.com>
2017-07-08 00:23:42 +0900

    new making
    새 서버 만들기

---
+++ ubuntu/app.js
@@ -0,0 +1,26 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() {
+		console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
+
+var Book = require('./models/book');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = require('./router')(app, Book);
+
+var server = app.listen(port, function() {
+		console.log("Express server has started on port " + port)
+});
